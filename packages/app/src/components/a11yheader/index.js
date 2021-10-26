8bf9a10f9bc3bbcbf00106f44220d5dc0664947a
vitorsanc-beuni <vitor.santana@beuni.com.br>
2021-10-26 00:47:12 -0300

    fix(components/a11y-header): make the high contrast button work only when clicked

--- packages/app/src/components/a11yheader/index.js
+++ packages/app/src/components/a11yheader/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import './contrast.js';
+import React, { useState, useEffect } from "react";
 import { Nav, NavItem, NavLink } from 'reactstrap';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./high-contrast-actions.css";
 import "./high-contrast-backgrounds.css";
@@ -9,82 +9,63 @@ import "./high-contrast-images.css";
 import "./high-contrast-texts.css";
 import "./style.css";
 
+const storageKey = 'isAtHighContrast'
+const cssContrastClass = 'contrast'
+
 const A11yHeader = () => {
-  (function () {
-    let Contrast = {
-        storage: 'contrastState',
-        cssClass: 'contrast',
-        currentState: null,
-        check: checkContrast,
-        getState: getContrastState,
-        setState: setContrastState,
-        toogle: toogleContrast,
-        updateView: updateViewContrast
-    };
+  // NOTE: HC = High Contrast
+
+  (function() {
+    const Contrast = {
+      isAtHC: false,
+      toggle: function () {
+        this.setContrast(!this.isAtHC);
+      },
+      setContrast: function (isAtHighContrast) {
+        localStorage[storageKey] = '' + isAtHighContrast;
+        this.isAtHC = isAtHighContrast;
+        this.updateSiteBody();
+      },
+      updateSiteBody: function () {
+        const body = document.body;
+        if (!body) return;
+        
+        this.isAtHC = this.isAtHC || localStorage[storageKey] === 'true';
+        
+        if (this.isAtHC) {
+          body.classList.add(cssContrastClass);
+        }
+        else {
+          body.classList.remove(cssContrastClass);
+        }
+      }
+    }
 
     window.toggleContrast = function () {
-      console.log("TOGGLANDO")
-      Contrast.toogle(); 
+      Contrast.toggle(); 
     };
 
-    Contrast.check();
-
-    function checkContrast() {
-        this.updateView();
-    }
-
-    function getContrastState() {
-        return localStorage.getItem(this.storage) === 'true';
-    }
-
-    function setContrastState(state) {
-        localStorage.setItem(this.storage, '' + state);
-        this.currentState = state;
-        this.updateView();
-    }
-
-    function updateViewContrast() {
-        let body = document.body;
-        console.log("CONTRASTANTOOOOOOO", body)
-        
-        if (!body) {
-          console.log("NOBODY")
-          return;
-        }
-        else{
-          console.log("YESBODY", this.currentState)
-        }
-
-        if (this.currentState === null)
-            this.currentState = this.getState();
-            console.log("NEW STATE", this.currentState)
-
-        if (this.currentState){
-            console.log("CLASSE ADICIONADAAAA =D")
-            body.classList.add(this.cssClass);}
-        else{
-            console.log("CLASSE NÃÃÃÃÃO ADICIONADAAAA T_T")
-            body.classList.remove(this.cssClass);
-        }
-    }
-
-    function toogleContrast() {
-        console.log("TOGGLE CONTRAST")
-        this.setState(!this.currentState);
-    }
-  })(); 
-
+    Contrast.updateSiteBody();
+  })();
+  
   return (
     <Nav className="a11ybar-content justify-content-end">
-            <NavItem class="nav-item">
-              <NavLink  class="nav-link" href="#about" accesskey="1">Ir para o Conteúdo Principal [Alt + 1]</NavLink>
-            </NavItem>
-            <NavItem class="nav-item">
-              <NavLink  class="nav-link" href="#home-header" accesskey="2">Início do Menu [Alt + 2]</NavLink>
-            </NavItem>
-            <NavItem class="nav-item">
-              <NavLink  class="nav-link" href="#altocontraste" id="altocontraste" accesskey="3" onkeydown={window.toggleContrast()}>Alto contraste [Alt + 3]</NavLink>
-            </NavItem>
+      <NavItem class="nav-item">
+        <NavLink  class="nav-link" href="#about" accesskey="1">Ir para o Conteúdo Principal [Alt + 1]</NavLink>
+      </NavItem>
+      <NavItem class="nav-item">
+        <NavLink  class="nav-link" href="#home-header" accesskey="2">Início do Menu [Alt + 2]</NavLink>
+      </NavItem>
+      <NavItem class="nav-item">
+        <NavLink 
+          class="nav-link" 
+          href="#altocontraste" 
+          id="altocontraste" 
+          accesskey="3" 
+          onClick={() => { window.toggleContrast() }}
+          onKeyDown={() => { window.toggleContrast() }}
+        >Alto contraste [Alt + 3]</NavLink>
+      </NavItem>
     </Nav>
   );
 };
