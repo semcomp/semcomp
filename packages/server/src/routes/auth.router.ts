cf2ceecae4a9b56a8b013f8d0b6b943b419c386b
Rafael Doering <rafael.doering.soares@usp.br>
2022-05-29 16:16:44 -0300

    Altera tamanho minimo da senha para 8 caracteres

--- packages/server/src/routes/auth.router.ts
+++ packages/server/src/routes/auth.router.ts
@@ -11,7 +11,7 @@ router.post(
   [
     body("name", "Invalid field 'name'").not().isEmpty(),
     body("email", "Invalid field 'email'").isEmail(),
-    body("password", "Invalid field 'password'").isLength({ min: 6 }),
+    body("password", "Invalid field 'password'").isLength({ min: 8 }),
     body("permission", "Invalid field 'permission'").isBoolean(),
   ],
   AuthController.signup
@@ -31,7 +31,7 @@ router.post(
   "/login",
   [
     body("email", "Invalid field 'email'").isEmail(),
-    body("password", "Invalid field 'password'").isLength({ min: 6 }),
+    body("password", "Invalid field 'password'").isLength({ min: 8 }),
   ],
   AuthController.login
 );
@@ -47,7 +47,7 @@ router.post(
   [
     body("email", "Invalid field 'email'").isEmail(),
     body("code", "Invalid field 'code'").isLength({ min: 12, max: 12 }),
-    body("password", "Invalid field 'password'").isLength({ min: 6 }),
+    body("password", "Invalid field 'password'").isLength({ min: 8 }),
   ],
   AuthController.resetPassword
 );
