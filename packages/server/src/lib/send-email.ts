1d0a9797da11774e456fabe7940657b9000796f3
Rafael Doering <rafael.doering.soares@usp.br>
2022-05-30 01:31:52 -0300

    Bugfix send email

--- packages/server/src/lib/send-email.ts
+++ packages/server/src/lib/send-email.ts
@@ -3,19 +3,22 @@ import nodemailer from "nodemailer";
 const { env } = process;
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: 'gmail',
   auth: {
-    user: env.EMAIL,
-    pass: env.PASSWORD,
-  },
+    type: 'OAuth2',
+    user: env.GMAIL_EMAIL,
+    clientId: env.GMAIL_CLIENT_ID,
+    clientSecret: env.GMAIL_CLIENT_SECRET,
+    refreshToken: env.GMAIL_REFRESH_TOKEN,
+  }
 });
 
 export const sendEmail = async function (to, subject, text, html) {
-  // await transporter.sendMail({
-  //   from: '"Semcomp ❤" <noreply.semcomp@gmail.com>',
-  //   to,
-  //   subject,
-  //   text,
-  //   html,
-  // });
+  await transporter.sendMail({
+    from: '"Semcomp ❤" <noreply.semcomp@gmail.com>',
+    to,
+    subject,
+    text,
+    html,
+  });
 };
