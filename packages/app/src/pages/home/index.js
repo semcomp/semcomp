d49a3732a675ee01c41f701d2b47094efe8ecb62
henriquesqs <henriquesqs@hotmail.com>
2022-06-06 20:40:05 -0300

    Add FAQ to homepage

--- packages/app/src/pages/home/index.js
+++ packages/app/src/pages/home/index.js
@@ -24,7 +24,7 @@ function Home() {
         {/* <Schedule /> */}
         <AboutBeta />
         {/* <Sponsors /> */}
-        {/* <FAQ /> */}
+        <FAQ />
         <Footer />
       </div>
     </main>
