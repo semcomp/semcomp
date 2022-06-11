34542f4fe82f477cb5baacb42fa4061f954f8b6a
Rafael Doering <rafael.doering.soares@usp.br>
2022-06-11 15:11:26 -0300

    Atualiza logo do ucl na pagina de patrocinadores

--- packages/app/src/pages/sponsors/index.js
+++ packages/app/src/pages/sponsors/index.js
@@ -8,10 +8,7 @@ import SponsorCard from "./sponsor-card";
 
 import IcmcImage from "../../assets/sponsors/icmc50.png";
 import FogImage from "../../assets/sponsors/fog.png";
-import CodelabImage from "../../assets/sponsors/codelab.svg";
-import GaneshImage from "../../assets/sponsors/ganesh.png";
-import JuniorImage from "../../assets/sponsors/junior.png";
-import GemaImage from "../../assets/sponsors/gema.png";
+import CodelabImage from "../../assets/sponsors/logo-codelab-sanca.svg";
 import PetImage from "../../assets/sponsors/pet.png";
 
 import "./style.css";
