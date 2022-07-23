13ed07d6752068a3acad71675addedac31a45d42
Rafael Doering <rafael.doering.soares@usp.br>
2022-07-22 22:17:15 -0300

    Refatora log controller

--- packages/server/src/routes/admin/log.router.ts
+++ packages/server/src/routes/admin/log.router.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 
 import adminAuthMiddleware from "../../middlewares/admin-auth.middleware";
-import * as AdminLogController from "../../controllers/admin/log.controller";
+import adminLogController from "../../controllers/admin/log.controller";
 
 const router = Router();
 
 router.get(
   "/",
   [adminAuthMiddleware.authenticate, adminAuthMiddleware.isAuthenticated],
-  AdminLogController.list
+  adminLogController.list
 );
 
 export default router;
