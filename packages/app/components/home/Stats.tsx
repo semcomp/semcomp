bbb698f60008f6b2b976cf37f4a49b024a584089
rafaeldoering <rafael.doering.soares@usp.br>
2022-08-25 00:01:15 -0300

    Refatora stats

---
+++ packages/app/components/home/Stats.tsx
@@ -0,0 +1,20 @@
+function Stats() {
+  return (<>
+    <section className="flex flex-col items-center text-primary bg-white text-center p-16">
+      <div className="flex justify-around w-full text-2xl font-bold max-w-4xl">
+        <p>
+          +2000
+          <br />
+          Participantes
+        </p>
+        <p>
+          24
+          <br />
+          Edições
+        </p>
+      </div>
+    </section>
+  </>);
+}
+
+export default Stats;
--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -5,7 +5,7 @@ import HomeHeader from "../components/home/Header";
 // import FAQ from "../components/home/Faq";
 // import LiveNow from "../components/home/live-now";
 // import Sponsors from "../components/home/Sponsors";
-// import Stats from "../components/home/stats";
+// import Stats from "../components/home/Stats";
 
 function Home() {
   return (
