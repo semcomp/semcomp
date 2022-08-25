12c72012598c052004ec28398f526721315dfab3
rafaeldoering <rafael.doering.soares@usp.br>
2022-08-24 22:05:12 -0300

    Refatora sobre

--- packages/app/components/home/About.tsx
+++ packages/app/components/home/About.tsx
@@ -1,20 +1,22 @@
 function AboutBeta() {
-  return (
-    <>
-      <section className="home-about-beta" id="about">
-        <h1>Sobre a Semcomp Beta</h1>
+  return (<>
+    <section className="flex flex-col items-center text-primary bg-white text-center p-16">
+      <h1 className="text-4xl font-bold">Sobre a Semcomp Beta</h1>
+      <div className="text-base pt-8 max-w-4xl">
         <p>
           A <strong>Semcomp</strong> é a <strong>Semana Acadêmica de Computação</strong>, realizada pelos cursos de <strong>Ciências de Computação</strong> e de <strong>Sistemas de Informação</strong> do Instituto de Ciências Matemáticas e de Computação (ICMC) da USP de São Carlos.
         </p>
+        <br />
         <p>
           A prévia da nossa 25ª edição, a Semcomp Beta, <strong>ocorrerá nos dias 11 e 12 de junho de 2022</strong> de forma presencial no ICMC. Nossa 25ª edição, por sua vez, acontecerá entre os dias 26 e 30 de setembro de 2022 e também será realizada de forma presencial no ICMC.
         </p>
+        <br />
         <p>
           Não deixe de participar e prestigiar o grande retorno da Semcomp ao presencial!
         </p>
-      </section>
-    </>
-  );
+      </div>
+    </section>
+  </>);
 }
 
 export default AboutBeta;
