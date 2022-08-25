d2ec5179ab6f3b045635f9cdc2355b89e5db13e7
rafaeldoering <rafael.doering.soares@usp.br>
2022-08-24 22:05:50 -0300

    Remove css's não utilizados

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -1,11 +1,10 @@
-// import About from "../components/home/about";
-// import AboutBeta from "../components/home/about-beta";
+// import About from "../components/home/About";
 // import Footer from "../components/footer";
 import HomeHeader from "../components/home/Header";
-// import Schedule from "../components/home/schedule/Schedule";
+// import Schedule from "../components/home/Schedule";
 // import FAQ from "../components/home/faq";
 // import LiveNow from "../components/home/live-now";
-// import Sponsors from "../components/home/sponsors";$
+// import Sponsors from "../components/home/sponsors";
 // import Stats from "../components/home/stats";
 
 function Home() {
@@ -16,11 +15,10 @@ function Home() {
         <HomeHeader />
         {/* <Stats /> */}
         {/* <About /> */}
-        {/* <Schedule /> */}
-        {/* <AboutBeta /> */}
-        {/* <Sponsors />
-        <FAQ />
-        <Footer /> */}
+        {/* <Schedule />
+        <Sponsors /> */}
+        {/* <FAQ /> */}
+        {/* <Footer /> */}
       </div>
     </main>
   );
