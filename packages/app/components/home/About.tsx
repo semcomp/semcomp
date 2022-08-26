a228d6f7bdaa854908db149562528ab477eb3cd0
henriquesqs <henriquesqs@hotmail.com>
2022-08-26 15:14:10 -0300

    feat: atualize textos da seção Sobre

--- packages/app/components/home/About.tsx
+++ packages/app/components/home/About.tsx
@@ -1,18 +1,18 @@
 function AboutBeta() {
   return (<>
     <section className="flex flex-col items-center text-primary bg-white text-center p-16">
-      <h1 className="text-4xl font-bold">Sobre a Semcomp Beta</h1>
+      <h1 className="text-4xl font-bold">Sobre a Semcomp</h1>
       <div className="text-base pt-8 max-w-4xl">
         <p>
-          A <strong>Semcomp</strong> é a <strong>Semana Acadêmica de Computação</strong>, realizada pelos cursos de <strong>Ciências de Computação</strong> e de <strong>Sistemas de Informação</strong> do Instituto de Ciências Matemáticas e de Computação (ICMC) da USP de São Carlos.
+          A <strong>Semcomp</strong> é a <strong>Semana Acadêmica de Computação</strong>, realizada pelos cursos de <strong>Ciências de Computação</strong>, de <strong>Sistemas de Informação</strong> e <strong>Ciência de Dados</strong> do Instituto de Ciências Matemáticas e de Computação (ICMC) da USP de São Carlos.
         </p>
         <br />
         <p>
-          A prévia da nossa 25ª edição, a Semcomp Beta, <strong>ocorrerá nos dias 11 e 12 de junho de 2022</strong> de forma presencial no ICMC. Nossa 25ª edição, por sua vez, acontecerá entre os dias 26 e 30 de setembro de 2022 e também será realizada de forma presencial no ICMC.
+          A sua 25ª edição <strong>ocorrerá entre os dias 24 e 30 de Setembro de 2022</strong> de forma híbrida, com transmissões parciais pelo Youtube.
         </p>
         <br />
         <p>
-          Não deixe de participar e prestigiar o grande retorno da Semcomp ao presencial!
+          Não deixe de participar, basta criar sua conta!
         </p>
       </div>
     </section>
