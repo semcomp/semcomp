1ffa63e58eaaa1fa2f90c34631620bad9174cd9f
guijun13 <guilhermejun13.grillo@gmail.com>
2022-08-27 18:16:27 -0300

    fix: add document.tsx to fix styling imports

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
+import { useEffect, useState } from "react";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import { ToastContainer, toast } from "react-toastify";
 
@@ -9,7 +9,7 @@ import baseURL from "../constants/api-url";
 import { AppContext } from "../libs/contextLib";
 
 import "react-toastify/dist/ReactToastify.css";
-import '../styles/globals.css';
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [user, setUser] = useState(null);
@@ -36,18 +36,16 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, []);
 
   return (
-    <AppContext.Provider value={{
-      user,
-      setUser,
-      token,
-      setToken
-    }}>
+    <AppContext.Provider
+      value={{
+        user,
+        setUser,
+        token,
+        setToken,
+      }}
+    >
       <Head>
         <meta charSet="utf-8" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-          rel="stylesheet"
-        />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
@@ -55,10 +53,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="theme-color" content="#000000" />
         <meta name="description" content="Semcomp site" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;700&family=Poppins&display=swap"
-          rel="stylesheet"
-        />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -77,10 +71,6 @@ function MyApp({ Component, pageProps }: AppProps) {
           href="/favicon/favicon-16x16.png"
         />
         <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
-          rel="stylesheet"
-        />
         <title>Semcomp</title>
       </Head>
       <ToastContainer hideProgressBar />
@@ -89,4 +79,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp
+export default MyApp;
---
+++ packages/app/pages/_document.tsx
@@ -0,0 +1,26 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;700&family=Poppins&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
