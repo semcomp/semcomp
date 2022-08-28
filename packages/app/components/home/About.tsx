9b5b3e9f102f9644437c2dd77d030ffaf6b6530b
guijun13 <guilhermejun13.grillo@gmail.com>
2022-08-28 11:04:35 -0300

    fix: add missing id for about and schedule sections

--- packages/app/components/home/About.tsx
+++ packages/app/components/home/About.tsx
@@ -1,22 +1,34 @@
 function AboutBeta() {
-  return (<>
-    <section className="flex flex-col items-center text-primary bg-white text-center p-16">
-      <h1 id="titulo" className="text-4xl font-bold">Sobre a Semcomp</h1>
-      <div className="text-base pt-8 max-w-4xl">
-        <p>
-          A <strong>Semcomp</strong> é a <strong>Semana Acadêmica de Computação</strong>, realizada pelos cursos de <strong>Ciências de Computação</strong>, de <strong>Sistemas de Informação</strong> e <strong>Ciência de Dados</strong> do Instituto de Ciências Matemáticas e de Computação (ICMC) da USP de São Carlos.
-        </p>
-        <br />
-        <p>
-          A sua 25ª edição <strong>ocorrerá entre os dias 24 e 30 de Setembro de 2022</strong> de forma híbrida, com transmissões parciais pelo Youtube.
-        </p>
-        <br />
-        <p>
-          Não deixe de participar, basta criar sua conta!
-        </p>
-      </div>
-    </section>
-  </>);
+  return (
+    <>
+      <section
+        id="about"
+        className="flex flex-col items-center text-primary bg-white text-center p-16"
+      >
+        <h1 id="titulo" className="text-4xl font-bold">
+          Sobre a Semcomp
+        </h1>
+        <div className="text-base pt-8 max-w-4xl">
+          <p>
+            A <strong>Semcomp</strong> é a{" "}
+            <strong>Semana Acadêmica de Computação</strong>, realizada pelos
+            cursos de <strong>Ciências de Computação</strong>, de{" "}
+            <strong>Sistemas de Informação</strong> e{" "}
+            <strong>Ciência de Dados</strong> do Instituto de Ciências
+            Matemáticas e de Computação (ICMC) da USP de São Carlos.
+          </p>
+          <br />
+          <p>
+            A sua 25ª edição{" "}
+            <strong>ocorrerá entre os dias 24 e 30 de Setembro de 2022</strong>{" "}
+            de forma híbrida, com transmissões parciais pelo Youtube.
+          </p>
+          <br />
+          <p>Não deixe de participar, basta criar sua conta!</p>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default AboutBeta;
--- packages/app/components/home/Schedule.tsx
+++ packages/app/components/home/Schedule.tsx
@@ -1,14 +1,21 @@
 import EventsCalendar from "../events-calendar/EventsCalendar";
 
 const Schedule = () => {
-  return (<>
-    <section className="flex flex-col items-center text-secondary bg-primary text-center p-16">
-      <h1 id="titulo" className="text-4xl font-bold">Cronograma</h1>
-      <div className="text-base pt-8 max-w-4xl">
-        <EventsCalendar />
-      </div>
-    </section>
-  </>);
+  return (
+    <>
+      <section
+        id="schedule"
+        className="flex flex-col items-center text-secondary bg-primary text-center p-16"
+      >
+        <h1 id="titulo" className="text-4xl font-bold">
+          Cronograma
+        </h1>
+        <div className="text-base pt-8 max-w-4xl">
+          <EventsCalendar />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Schedule;
