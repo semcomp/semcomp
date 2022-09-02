98cec71afbd2efb68b163149a6fca68fc67599f7
rafaeldoering <rafael.doering.soares@usp.br>
2022-09-01 23:59:21 -0300

    Corrige persistencia de sessão no backoffice

--- packages/app-admin-next/api/base-api.ts
+++ packages/app-admin-next/api/base-api.ts
@@ -1,22 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
-import { getDefaultMessage } from './error-message';
+import { getDefaultMessage } from "./error-message";
 
 let globalAPI;
 
-/**
- * @param {object} config
- *
- * @return {object}
- */
 function initializeAPI(config) {
   const {
     baseURL,
-    onBadToken = () => { },
     onError = () => { },
-    tokenDispatcher,
-    tokenSelector,
     timeout,
+    token,
+    setToken,
+    onBadToken,
   } = config;
 
   const api = axios.create({
@@ -26,36 +21,41 @@ function initializeAPI(config) {
 
   // Sends a token if there's one in redux
   api.interceptors.request.use((request) => {
-    const token = tokenSelector();
+    // If a token is already set, don't override it.
+    if (request.headers.authorization) return request;
     if (token) request.headers.authorization = token;
     return request;
   });
 
   // Updates token if 'Authorization' header is filled
   api.interceptors.response.use((response) => {
-    const token = response.headers.authorization || (response.data && response.data.token);
-    if (token) tokenDispatcher(token);
+    const newToken = response.headers.authorization;
+    if (newToken) {
+      setToken(newToken);
+      localStorage.setItem("token", newToken);
+    }
     return response;
   });
 
   // Handle errors
-  api.interceptors.response.use((r) => r, (error) => {
-    const response = error.response;
-    if (response && response.status === 401) onBadToken();
+  api.interceptors.response.use(
+    (r) => r,
+    (error) => {
+      const response = error.response;
+      if (response && response.status === 401) onBadToken();
 
-    if (response) response.errorMessage = response.errorMessage || getDefaultMessage(response);
+      setTimeout(() => {
+        if (!response) {
+          onError(getDefaultMessage());
+        } else {
+          const message = response.errorMessage || getDefaultMessage(response);
+          if (message !== "_NO_ERROR_MESSAGE") onError(message);
+        }
+      });
 
-    setTimeout(() => {
-      if (!response) {
-        onError(getDefaultMessage());
-      } else {
-        console.log('debug', response);
-        if (!response.shouldOmitError) onError(response.errorMessage);
-      }
-    });
-
-    throw response;
-  });
+      throw response;
+    }
+  );
 
   globalAPI = api;
   return api;
@@ -63,15 +63,16 @@ function initializeAPI(config) {
 
 /** This proxy is to make sure the API has been initialized before being accessed
 @type { import('axios').AxiosInstance } */
-const APIProtectorProxy = new Proxy({}, {
-  get(target, prop) {
-    // React will try to access this property to determine if this is a React component or not.
-    // This is not a component, therefore, a null is returned
-    if (prop === '$$typeof') return null;
-    if (!globalAPI) throw new Error('Cannot access API before it\'s initialization. Please call `initializeAPI()` before using this import.');
-    else return globalAPI[prop];
-  },
-});
+const APIProtectorProxy = new Proxy(
+  {},
+  {
+    get(target, prop) {
+      if (!globalAPI)
+        throw new Error("Cannot access API before it's initialization");
+      else return globalAPI[prop];
+    },
+  }
+);
 
 export default APIProtectorProxy;
 export { initializeAPI };
