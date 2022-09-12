70be1a92c85ad10c9357f109e7f840e84a10f705
rafaeldoering <rafael.doering.soares@usp.br>
2022-09-11 23:54:42 -0300

    Corrige envio de emails

--- packages/server/src/services/email.service.ts
+++ packages/server/src/services/email.service.ts
@@ -1,12 +1,24 @@
 import nodemailer from "nodemailer";
+import { google } from "googleapis";
+import { OAuth2Client } from "googleapis-common";
 
 const { env } = process;
 
 class EmailService {
-  private transporter: nodemailer.Transporter;
+  private oAuth2Client: OAuth2Client;
 
   constructor() {
-    this.transporter = nodemailer.createTransport({
+    this.oAuth2Client = new google.auth.OAuth2(
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_SECRET,
+      process.env.GOOGLE_REDIRECT_URL,
+    );
+    this.oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
+  }
+
+  public async send(to: string, subject: string, text: string, html: string): Promise<void> {
+    const accessToken = await this.oAuth2Client.getAccessToken();
+    const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         type: 'OAuth2',
@@ -14,12 +26,11 @@ class EmailService {
         clientId: env.GMAIL_CLIENT_ID,
         clientSecret: env.GMAIL_CLIENT_SECRET,
         refreshToken: env.GMAIL_REFRESH_TOKEN,
+        accessToken,
       }
-    });
-  }
+    } as any);
 
-  public async send(to: string, subject: string, text: string, html: string): Promise<void> {
-    await this.transporter.sendMail({
+    await transporter.sendMail({
       from: '"Semcomp ❤" <noreply.semcomp@gmail.com>',
       to,
       subject,
