278ab8ffde39486f118907c6efd6cdd3616ca77d
guijun13 <guilhermejun13.grillo@gmail.com>
2022-09-27 11:13:55 -0300

    update backoffice for not breaking in /events

--- packages/app-admin-next/components/reusable/DataTable.tsx
+++ packages/app-admin-next/components/reusable/DataTable.tsx
@@ -1,18 +1,24 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode, useState } from "react";
 
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { Box, Collapse, IconButton, TablePagination, Typography } from '@mui/material';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import {
+  Box,
+  Collapse,
+  IconButton,
+  TablePagination,
+  Typography,
+} from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
-import Input, { InputType } from '../Input';
-import { PaginationRequest, PaginationResponse } from '../../models/Pagination';
+import Input, { InputType } from "../Input";
+import { PaginationRequest, PaginationResponse } from "../../models/Pagination";
 
 function Row({
   index,
@@ -22,12 +28,12 @@ function Row({
   moreInfoContainer,
   onMoreInfoClick,
 }: {
-  index: number,
-  row: any,
-  onClick: (index: number) => void,
-  onSelectChange: (isSelected: boolean) => void,
-  moreInfoContainer: ReactNode,
-  onMoreInfoClick: (index: number) => void,
+  index: number;
+  row: any;
+  onClick: (index: number) => void;
+  onSelectChange: (isSelected: boolean) => void;
+  moreInfoContainer: ReactNode;
+  onMoreInfoClick: (index: number) => void;
 }) {
   const [isSelected, setIsSelected] = useState(false);
   const [open, setOpen] = useState(false);
@@ -37,51 +43,55 @@ function Row({
     setIsSelected(!isSelected);
   }
 
-  return (<>
-    <TableRow
-      hover={true}
-      sx={{ '& > *': { borderBottom: 'unset' } }}
-      className="hover:cursor-pointer"
-    >
-      {
-        moreInfoContainer && (<TableCell>
-          <IconButton
-            aria-label="expand row"
-            size="small"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
-        </TableCell>)
-      }
-      <TableCell>
-        <Input
-          className="my-3"
-          onChange={handleOnSelect}
-          value={isSelected}
-          type={InputType.Checkbox}
-        />
-      </TableCell>
-      {Object.values(row).map((value: any, index: number) => {
-        return (<TableCell key={index} onClick={() => onClick(index)}>{value}</TableCell>);
-      })}
-    </TableRow>
-    {
-      moreInfoContainer && (<TableRow
+  return (
+    <>
+      <TableRow
         hover={true}
-        onClick={() => onMoreInfoClick(index)}
+        sx={{ "& > *": { borderBottom: "unset" } }}
         className="hover:cursor-pointer"
       >
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              {moreInfoContainer}
-            </Box>
-          </Collapse>
+        {moreInfoContainer && (
+          <TableCell>
+            <IconButton
+              aria-label="expand row"
+              size="small"
+              onClick={() => setOpen(!open)}
+            >
+              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+            </IconButton>
+          </TableCell>
+        )}
+        <TableCell>
+          <Input
+            className="my-3"
+            onChange={handleOnSelect}
+            value={isSelected}
+            type={InputType.Checkbox}
+          />
         </TableCell>
-      </TableRow>)
-    }
-  </>);
+        {Object.values(row).map((value: any, index: number) => {
+          return (
+            <TableCell key={index} onClick={() => onClick(index)}>
+              {value}
+            </TableCell>
+          );
+        })}
+      </TableRow>
+      {moreInfoContainer && (
+        <TableRow
+          hover={true}
+          onClick={() => onMoreInfoClick(index)}
+          className="hover:cursor-pointer"
+        >
+          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <Box sx={{ margin: 1 }}>{moreInfoContainer}</Box>
+            </Collapse>
+          </TableCell>
+        </TableRow>
+      )}
+    </>
+  );
 }
 
 export default function DataTable({
@@ -92,12 +102,12 @@ export default function DataTable({
   moreInfoContainer,
   onMoreInfoClick,
 }: {
-  data: PaginationResponse<any>,
-  pagination: PaginationRequest,
-  onRowClick: (index: number) => void,
-  onRowSelect: (indexes: number[]) => void,
-  moreInfoContainer?: ReactNode,
-  onMoreInfoClick?: (index: number) => void,
+  data: PaginationResponse<any>;
+  pagination: PaginationRequest;
+  onRowClick: (index: number) => void;
+  onRowSelect: (indexes: number[]) => void;
+  moreInfoContainer?: ReactNode;
+  onMoreInfoClick?: (index: number) => void;
 }) {
   const [selectedRows, setSelectedRows] = useState([]);
 
@@ -109,7 +119,7 @@ export default function DataTable({
     } else {
       updatedSelectedRows = updatedSelectedRows.filter((index) => {
         return index !== selectChangeIndex;
-      })
+      });
     }
 
     setSelectedRows(updatedSelectedRows);
@@ -118,54 +128,60 @@ export default function DataTable({
 
   function handleChangePage(
     _event: React.MouseEvent<HTMLButtonElement> | null,
-    newPage: number,
+    newPage: number
   ) {
     pagination.setPage(newPage + 1);
-  };
+  }
 
   function handleChangeRowsPerPage(
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) {
     pagination.setItems(parseInt(event.target.value, 10));
-  };
+  }
 
-  return (<>
-    {
-      data.getEntities()[0] &&
-      <TableContainer component={Paper}>
-        <Table aria-label="collapsible table">
-          <TableHead>
-            <TableRow>
-              { moreInfoContainer && <TableCell></TableCell>}
-              <TableCell></TableCell>
-              {Object.keys(data.getEntities()[0]).map((key: any, index: number) => {
-                return (<TableCell key={index}>{key}</TableCell>);
-              })}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.getEntities().map((row, index) => (
-              <Row
-                key={index}
-                index={index}
-                row={row}
-                onClick={() => onRowClick(index)}
-                onSelectChange={(isSelected) => handleRowSelect(index, isSelected)}
-                moreInfoContainer={moreInfoContainer}
-                onMoreInfoClick={onMoreInfoClick}
-              />
-            ))}
-          </TableBody>
-        </Table>
-        <TablePagination
-          component="div"
-          count={data.getTotalNumberOfItems()}
-          page={pagination.getPage() - 1}
-          onPageChange={handleChangePage}
-          rowsPerPage={pagination.getItems()}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </TableContainer>
-    }
-  </>);
+  return (
+    <>
+      {data.getEntities()[0] && (
+        <TableContainer component={Paper}>
+          <Table aria-label="collapsible table">
+            <TableHead>
+              <TableRow>
+                {moreInfoContainer && <TableCell></TableCell>}
+                <TableCell></TableCell>
+                {Object.keys(data.getEntities()[0]).map(
+                  (key: any, index: number) => {
+                    return <TableCell key={index}>{key}</TableCell>;
+                  }
+                )}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.getEntities().map((row, index) => (
+                <Row
+                  key={index}
+                  index={index}
+                  row={row}
+                  onClick={() => onRowClick(index)}
+                  onSelectChange={(isSelected) =>
+                    handleRowSelect(index, isSelected)
+                  }
+                  moreInfoContainer={moreInfoContainer}
+                  onMoreInfoClick={onMoreInfoClick}
+                />
+              ))}
+            </TableBody>
+          </Table>
+          <TablePagination
+            component="div"
+            count={data.getTotalNumberOfItems()}
+            page={pagination.getPage() - 1}
+            onPageChange={handleChangePage}
+            rowsPerPage={pagination.getItems()}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            rowsPerPageOptions={[5, 10, 25, 50, 100]}
+          />
+        </TableContainer>
+      )}
+    </>
+  );
 }
