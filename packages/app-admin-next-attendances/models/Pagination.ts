8c46808c4e4b4760acf33579d9ecca6cb44c2247
rafaeldoering <rafael.doering.soares@usp.br>
2022-09-30 09:27:33 -0300

    Diminui paginação

--- packages/app-admin-next-attendances/models/Pagination.ts
+++ packages/app-admin-next-attendances/models/Pagination.ts
@@ -11,7 +11,7 @@ export class PaginationRequest {
       this.page = page
     }
 
-    this.items = 10;
+    this.items = 5;
     if (items) {
       this.items = items
     }
--- packages/app-admin-next/components/reusable/DataTable.tsx
+++ packages/app-admin-next/components/reusable/DataTable.tsx
@@ -178,7 +178,7 @@ export default function DataTable({
             onPageChange={handleChangePage}
             rowsPerPage={pagination.getItems()}
             onRowsPerPageChange={handleChangeRowsPerPage}
-            rowsPerPageOptions={[5, 10, 25, 50, 100]}
+            rowsPerPageOptions={[2, 5, 10, 25, 50, 100]}
           />
         </TableContainer>
       )}
