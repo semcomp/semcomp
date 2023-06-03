2f9db76d8538d78ea107a0c97eff4e8ee9dcd834
Rebeca <rebeca.vcarvalho78@gmail.com>
2023-06-03 12:24:23 -0300

    O BUTÃO 😫

--- packages/app/components/game/start.tsx
+++ packages/app/components/game/start.tsx
@@ -9,9 +9,11 @@ function GameStart({ gameConfig }: { gameConfig: GameConfig }) {
         <h1 className="text-4xl text-center pb-4">{gameConfig.getName()}</h1>
         {gameConfig.getDescription()}
         <div className="flex justify-center">
-          <button className="bg-tertiary text-white text-lg md:text-2xl m-2 p-2 md:p-4 rounded-2xl hover:bg-secondary hover:text-black w-fit">
-            <Link href={gameConfig.getRoutes()[GameRoutes.LOBBY]}>Jogar</Link>
-          </button>
+            <Link href={gameConfig.getRoutes()[GameRoutes.LOBBY]}>
+              <button className="bg-tertiary text-white text-lg md:text-2xl m-2 p-2 md:p-4 rounded-2xl hover:bg-secondary hover:text-black w-fit">
+              Jogar
+              </button>
+            </Link>
         </div>
       </div>
     </>
