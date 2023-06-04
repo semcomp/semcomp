399ba9605ad99fb5beac48a24e870a7e73f052ba
Rebeca <rebeca.vcarvalho78@gmail.com>
2023-06-04 01:44:38 -0300

    Trying to fix router home problem

--- packages/app/pages/game/[game]/index.tsx
+++ packages/app/pages/game/[game]/index.tsx
@@ -17,6 +17,13 @@ import JoinTeam from '../../../components/game/join-team';
 export default function GamePage({children}) {
   const router = useRouter();
 
+  useEffect(() => {
+    console.log(window.location.pathname);
+    if (window.location.pathname != router.pathname) {
+      router.push(`/${window.location.pathname}`)
+    }
+  }, [])
+
   const { game } = router.query;
 
   const gameConfig = new GameConfig(game as string);
