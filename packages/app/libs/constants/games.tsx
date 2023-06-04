3fffcfa8c15c20bfa25806c3127ed49b8c2c4229
Rebeca <rebeca.vcarvalho78@gmail.com>
2023-06-04 15:23:58 -0300

    More time

--- packages/app/libs/constants/games.tsx
+++ packages/app/libs/constants/games.tsx
@@ -71,7 +71,7 @@ export const HARD_TO_CLICK: Game = {
   slug: 'duro-de-clicar',
   eventPrefix: 'hard-to-click',
   startDate: new Date("2023-06-04 15:00"),
-  endDate: new Date("2023-06-04 16:00"),
+  endDate: new Date("2023-06-04 16:10"),
   numberOfQuestions: 10,
   maximumNumberOfMembersInGroup: 3,
 }
