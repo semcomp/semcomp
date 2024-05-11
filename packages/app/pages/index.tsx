99bcdc5951f70352ddc35ae5b876a751c1a54015
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-05-10 22:09:08 -0300

    adicioando placa do icmc responsiva 🪧

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -30,9 +30,13 @@ function Home() {
     <main className="home  bg-black min-h-screen">
       <div>
         <section className="bg-[url('../assets/27-imgs/bgheadercontinue.png')] bg-repeat h-[857px]">
-          <section className="bg-[url('../assets/27-imgs/leftbgheader.png')] bg-no-repeat h-[857px]">
+          <section>
             <div className="relative">
-              <section className="absolute right-[10px] top-0 h-[857px] w-[650px] bg-[url('../assets/27-imgs/rightbgheader.png')] bg-no-repeat bg-cover"></section>
+              <section className="hidden lg:block absolute left-[0px] top-0 h-[857px] w-[450px] bg-[url('../assets/27-imgs/leftbgheader.png')] bg-no-repeat bg-cover"></section>
+              <section className="hidden lg:block absolute right-[10px] top-0 h-[857px] w-[590px] bg-[url('../assets/27-imgs/rightbgheader.png')] bg-no-repeat bg-cover"></section>
+
+              <section className="lg:hidden absolute left-0 top-0 h-[857px] w-[450px] bg-[url('../assets/27-imgs/placaICMC.png')] bg-no-repeat bg-cover"></section>
+
               <div className="relative h-[857px]">
                 <HomeHeader />
               </div>
