3f6b05b67905774f7f0c34ba0f6493f2c8e67519
Rebeca Carvalho <rebeca.vcarvalho78@gmail.com>
2024-09-07 16:57:41 -0300

    Fix url

--- packages/app-admin-next/constants/api-url.ts
+++ packages/app-admin-next/constants/api-url.ts
@@ -1,4 +1,4 @@
-export const baseURL = "http://localhost:8080";
-// export const baseURL = "https://semcomp.icmc.usp.br";
+// export const baseURL = "http://localhost:8080";
+export const baseURL = "https://semcomp.icmc.usp.br";
 
 export default baseURL + "/api";
