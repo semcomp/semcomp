aa08a682eba9a889782de21b72280b0f461678bc
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-09-25 14:28:20 -0300

    feat: add new logo

--- packages/app/components/home/TitleHome.tsx
+++ packages/app/components/home/TitleHome.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { ReactElement } from "react";
-import Logo from "../../components/home/Logo";
+import Image from "next/image";
+import logoSemcompona from "../../assets/27-imgs/semcomponaLogo.png"; // Importando a logo corretamente
 
 interface TitleHomeProps {
   timeIndex: number;
@@ -8,7 +9,7 @@ interface TitleHomeProps {
 
 const TitleHome: React.FC<TitleHomeProps> = ({ timeIndex }): ReactElement => {
   const [titleSize, setTitleSize] = useState<string>("6vw");
-  const [subTitleSize, setSubTitleSize] = useState<string>("1.5vw");
+  const [subTitleSize, setSubTitleSize] = useState<string>("2vw");
   const [subVisible, setSubVisible] = useState<boolean>(true);
   const [noAbsoluteMarginTop, setNoAbsoluteMarginTop] = useState<number | undefined>();
   const [betaMarginTop, setBetaMarginTop] = useState<number | undefined>();
@@ -22,9 +23,9 @@ const TitleHome: React.FC<TitleHomeProps> = ({ timeIndex }): ReactElement => {
       const width = window.innerWidth;
        // Lógica para o subtítulo
        if (width <= 660) {
-        setSubTitleFontSize("3vw");
+        setSubTitleFontSize("3.0vw");
       } else {
-        setSubTitleFontSize("1.5vw");
+        setSubTitleFontSize("2vw");
       }
 
       // Lógica para o título principal
@@ -68,46 +69,33 @@ const TitleHome: React.FC<TitleHomeProps> = ({ timeIndex }): ReactElement => {
 
   return (
     <header className="flex flex-col items-center justify-center w-full text-center text-black font-primary">
-       <div className="w-full z-4"></div>
+      <div className="w-full z-4"></div>
       <div style={{ display: showAccessorys ? "block" : "none" }}></div>
       <div
         className="relative text-center felz"
         style={{ marginTop: `${noAbsoluteMarginTop}px` }}
       >
-        <div>
-
-          <div
-            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[200px] h-[100px]"
-            style={{ marginTop: `${betaMarginTop}px` }}
-          ></div>
-          <h1
-            className="relative text-primary flex flex-col items-center justify-center md:mb-6 mobile:mb-8 text-center"
-            style={{
-              fontSize: titleFontSize, // Usando o estado para definir o tamanho do título principal
-              lineHeight: "0.7",
-              color: getTitleColor(timeIndex),
-              // textShadow: "2px 4px 6px rgba(36, 36, 36, 0.6)",
-              zIndex: 1,
-            }}
-          >
-            <Logo className="mobile:mb-6" width={logoSize} height={logoSize} fillColor={getTitleColor(timeIndex)}/>
-            SEMCOMP 27
-          </h1>
-        </div>
-          <p
-            className="relative text-primary mobile:hidden"
-            style={{
-              fontSize: subTitleFontSize, // Usando o estado para definir o tamanho do subtítulo
-              fontWeight: 100,
-              color: getTitleColor(timeIndex),
-              marginTop: "-0.5vw",
-              marginBottom: "1.5vw",
-              // textShadow: "1px 2px 4px rgba(36, 36, 36, 0.6)",
-              zIndex: 1,
-            }}
-          >
-            A maior semana acadêmica de computação do Brasil!
-          </p>
+        {/* Usando o componente Image do Next.js */}
+        <Image
+          src={logoSemcompona}
+          alt="Logo Semcompona"
+          width={500} // ajuste o valor conforme necessário
+          height={300} // ajuste o valor conforme necessário
+          style={{ width: logoSize, marginBottom: "15px"}}
+        />
+        <p
+          className="relative text-primary mobile:hidden"
+          style={{
+            fontSize: subTitleSize,
+            fontWeight: 100,
+            color: getTitleColor(timeIndex),
+            marginTop: "-0.5vw",
+            marginBottom: "1.5vw",
+            zIndex: 1,
+          }}
+        >
+          A maior semana acadêmica de computação do Brasil!
+        </p>
       </div>
     </header>
   );
