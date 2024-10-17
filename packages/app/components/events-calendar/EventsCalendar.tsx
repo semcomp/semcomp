a093fad4fa57553b90916e5096b670b25edacec6
ThierryAra <thierry_ara@usp.br>
2024-10-17 12:06:45 -0300

    fix: corrige bug no loading de EventsCalendar e reseta eventsExpanded ao trocar de dia

--- packages/app/components/events-calendar/EventsCalendar.tsx
+++ packages/app/components/events-calendar/EventsCalendar.tsx
@@ -38,6 +38,7 @@ function getUniqueDayAndDates(events: { startDate: string }[]) {
 
 const EventsCalendar = (props) => {
   const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [selectedDay, setSelectedDay] = useState(null);
   const [expandedEvents, setExpandedEvents] = useState([]);
   const [isMobile, setIsMobile] = useState(false);
@@ -63,6 +64,8 @@ const EventsCalendar = (props) => {
       } catch (e) {
         console.error(e);
         return [];
+      } finally {
+        setLoading(false);
       }
     }
 
@@ -79,7 +82,7 @@ const EventsCalendar = (props) => {
     fetchAndSetEvents();
   }, []);
 
-  if (events.length === 0) {
+  if (!loading && events.length === 0) {
     return (
       <p className="p-3 mt-8 mb-32 bg-white text-primary font-secondary rounded-xl bg-opacity-70">
         Por enquanto não temos nenhum evento divulgado!
@@ -102,158 +105,172 @@ const EventsCalendar = (props) => {
     });
   };
 
+  const changeDay = (day) => {
+    setExpandedEvents(new Array(events.length).fill(false));
+    setSelectedDay(day);
+  }
+
   return (
-<div className="w-full">
+    <div className="w-full">
       {/* Seletor de dias */}
-      {isMobile ? (
-        <div className="relative flex items-center justify-center mb-4 w-full">
-          <div className="relative w-48">
-            <select
-              value={selectedDay || ""}
-              onChange={(e) => setSelectedDay(e.target.value)}
-              className={`w-full px-4 py-2 text-white rounded-md font-secondary outline-none appearance-none transition ${
-                selectedDay ? "bg-purple-400" : "bg-secondary"
-              }`}
-            >
-              {uniqueDayAndDates.map((dayAndDate) => (
-                <option key={dayAndDate} value={dayAndDate}>
-                  {dayAndDate}
-                </option>
-              ))}
-            </select>
-
-            {/* Ícone de seta para baixo dentro do select */}
-            <AiOutlineDown className="absolute text-white transform -translate-y-1/2 pointer-events-none top-1/2 right-4" />
-          </div>
-        </div>
-      ) : (
-        <div className="flex justify-center mb-4 space-x-2">
-          {uniqueDayAndDates.map((dayAndDate) => (
-            <button
-              key={dayAndDate}
-              className={`px-4 py-2 rounded-md transition transform focus:outline-none ${
-                selectedDay === dayAndDate
-                  ? "bg-purple-400 text-gray-900 shadow-inner scale-95"
-                  : "bg-secondary text-gray-900 hover:bg-purple-400 active:scale-95 active:shadow-inner"
-              }`}
-              onClick={() => setSelectedDay(dayAndDate)}
-            >
-              {dayAndDate}
-            </button>
-          ))}
-        </div>
-      )}
-
-      {/* Timeline */}
-      <VerticalTimeline
-        animate={!isMobile}
-        lineColor={isMobile ? "transparent" : "white"}
-        className={`${isMobile ? "hidden-line" : ""}`}
-      >
-        {filteredEvents.map((event, index) => {
-          const startDateObj = new Date(event.startDate);
-          const endDateObj = new Date(event.endDate);
-
-          const startTimeStr = startDateObj.toLocaleTimeString("pt-BR", {
-            hour: "2-digit",
-            minute: "2-digit",
-          });
-          const endTimeStr = endDateObj.toLocaleTimeString("pt-BR", {
-            hour: "2-digit",
-            minute: "2-digit",
-          });
-
-          return (
-            <VerticalTimelineElement
-              key={event.id}
-              icon={isMobile ? null : <EventIcon />}
-              iconStyle={
-                isMobile
-                  ? { display: "none" }
-                  : { background: "rgb(192 132 252)", color: "#fff" }
-              }
-              contentStyle={
-                isMobile
-                  ? {
-                      background: "#1f2937",
-                      color: "#fff",
-                      boxShadow: "0 3px 6px rgba(0, 0, 0, 0.16)",
-                      maxWidth: "100%",
-                      marginLeft: "0",
-                      marginRight: "0",
-                    }
-                  : {
-                      background: "#1f2937",
-                      color: "#fff",
-                      boxShadow: "0 3px 6px rgba(0, 0, 0, 0.16)",
-                      maxWidth: "100%",
-                    }
-              }
-              contentArrowStyle={
-                isMobile
-                  ? { display: "none" }
-                  : { borderRight: "7px solid  #1f2937" }
-              }
-              className={`${isMobile ? "hidden-icon" : ""} font-secondary`}
-            >
-              <div onClick={() => toggleExpandEvent(index)}>
-                <div className="flex flex-row align-left justify-left w-full">
-                  <h4 className="text-purple-500 text-left pr-2 whitespace-nowrap">
-                      {event.type + " |"}
-                  </h4>
-                  <h3 className="text-left">
-                    {event.name}
-                  </h3>
+      { loading ?
+        <p>CARREGANDO</p>
+        : 
+        <>
+          {isMobile ? (
+            <>
+              <div className="relative flex items-center justify-center mb-4 w-full">
+                <div className="relative w-48">
+                  <select
+                    value={selectedDay || ""}
+                    onChange={(e) => changeDay(e.target.value)}
+                    className={`w-full px-4 py-2 text-white rounded-md font-secondary outline-none appearance-none transition ${
+                      selectedDay ? "bg-purple-400" : "bg-secondary"
+                    }`}
+                  >
+                    {uniqueDayAndDates.map((dayAndDate) => (
+                      <option key={dayAndDate} value={dayAndDate}>
+                        {dayAndDate}
+                      </option>
+                    ))}
+                  </select>
+                  
+                  {/* Ícone de seta para baixo dentro do select */}
+                  <AiOutlineDown className="absolute text-white transform -translate-y-1/2 pointer-events-none top-1/2 right-4" />
                 </div>
-                <p className="flex items-center text-yellow-400">
-                  <AiOutlineClockCircle className="mr-2" />
-                  {startTimeStr} - {endTimeStr}
-                </p>
               </div>
-              <div
-                className={`transition-max-height duration-700 ease-in-out overflow-hidden ${
-                  expandedEvents[index] ? "max-h-screen" : "max-h-0"
-                }`}
-              >
-                <div className="p-4">
-                  {event.speaker && event.speaker.length > 0 && 
-                    <div className="w-full flex flex-row items-center justify-left">
-                      <p className="text-purple-400 pr-2">Ministrante: </p> 
-                      <p>{event.speaker}</p>
-                    </div>
+            </>
+          ) : (
+            <>
+              <div className="flex justify-center mb-4 space-x-2">
+                {uniqueDayAndDates.map((dayAndDate) => (
+                  <button
+                    key={dayAndDate}
+                    className={`px-4 py-2 rounded-md transition transform focus:outline-none ${
+                      selectedDay === dayAndDate
+                        ? "bg-purple-400 text-gray-900 shadow-inner scale-95"
+                        : "bg-secondary text-gray-900 hover:bg-purple-400 active:scale-95 active:shadow-inner"
+                    }`}
+                    onClick={() => changeDay(dayAndDate)}
+                  >
+                    {dayAndDate}
+                  </button>
+                ))}
+              </div>
+            </>
+          )}
+          <VerticalTimeline
+            animate={!isMobile}
+            lineColor={isMobile ? "transparent" : "white"}
+            className={`${isMobile ? "hidden-line" : ""}`}
+          >
+            {filteredEvents.map((event, index) => {
+              const startDateObj = new Date(event.startDate);
+              const endDateObj = new Date(event.endDate);
+            
+              const startTimeStr = startDateObj.toLocaleTimeString("pt-BR", {
+                hour: "2-digit",
+                minute: "2-digit",
+              });
+              const endTimeStr = endDateObj.toLocaleTimeString("pt-BR", {
+                hour: "2-digit",
+                minute: "2-digit",
+              });
+            
+              return (
+                <VerticalTimelineElement
+                  key={event.id}
+                  icon={isMobile ? null : <EventIcon />}
+                  iconStyle={
+                    isMobile
+                      ? { display: "none" }
+                      : { background: "rgb(192 132 252)", color: "#fff" }
                   }
-                  {event.location && event.location.length > 0 && 
-                    <div className="w-full flex flex-row items-center justify-left">
-                        <p className="text-purple-400 pr-2">Local: </p>
-                        <p>{event.location}</p>
-                    </div>
+                  contentStyle={
+                    isMobile
+                      ? {
+                          background: "#1f2937",
+                          color: "#fff",
+                          boxShadow: "0 3px 6px rgba(0, 0, 0, 0.16)",
+                          maxWidth: "100%",
+                          marginLeft: "0",
+                          marginRight: "0",
+                        }
+                      : {
+                          background: "#1f2937",
+                          color: "#fff",
+                          boxShadow: "0 3px 6px rgba(0, 0, 0, 0.16)",
+                          maxWidth: "100%",
+                        }
                   }
-
-                  <p className="text-left whitespace-pre-line">{event.description}</p>
-
-
-                  {event.type === "Contest" && event.isSubscribed && (
-                    <button
-                      className="p-2 mt-2 text-white bg-red-600 shadow-md hover:bg-red-500"
-                      onClick={() =>
-                        window.open(
-                          event.link,
-                          "_blank",
-                          "noopener noreferrer"
-                        )
+                  contentArrowStyle={
+                    isMobile
+                      ? { display: "none" }
+                      : { borderRight: "7px solid  #1f2937" }
+                  }
+                  className={`${isMobile ? "hidden-icon" : ""} font-secondary`}
+                >
+                  <div onClick={() => toggleExpandEvent(index)}>
+                    <div className="flex flex-row align-left justify-left w-full">
+                      <h4 className="text-purple-500 text-left pr-2 whitespace-nowrap">
+                          {event.type + " |"}
+                      </h4>
+                      <h3 className="text-left">
+                        {event.name}
+                      </h3>
+                    </div>
+                    <p className="flex items-center text-yellow-400">
+                      <AiOutlineClockCircle className="mr-2" />
+                      {startTimeStr} - {endTimeStr}
+                    </p>
+                  </div>
+                  <div
+                    className={`transition-max-height duration-700 ease-in-out overflow-hidden ${
+                      expandedEvents[index] ? "max-h-screen" : "max-h-0"
+                    }`}
+                    style={{ transition: 'max-height 0.7s ease-in-out' }}
+                  >
+                    <div className="p-4">
+                      {event.speaker && event.speaker.length > 0 && 
+                        <div className="w-full flex flex-row items-center justify-left">
+                          <p className="text-purple-400 pr-2">Ministrante: </p> 
+                          <p>{event.speaker}</p>
+                        </div>
                       }
-                    >
-                      Entrar no contest
-                    </button>
-                  )}
-                </div>
-              </div>
-            </VerticalTimelineElement>
-          );
-        })}
-      </VerticalTimeline>
-    </div>
-  );
+                      {event.location && event.location.length > 0 && 
+                        <div className="w-full flex flex-row items-center justify-left">
+                            <p className="text-purple-400 pr-2">Local: </p>
+                            <p>{event.location}</p>
+                        </div>
+                      }
+
+                      <p className="text-left whitespace-pre-line">{event.description}</p>
+                    
+                    
+                      {event.type === "Contest" && event.isSubscribed && (
+                        <button
+                          className="p-2 mt-2 text-white bg-red-600 shadow-md hover:bg-red-500"
+                          onClick={() =>
+                            window.open(
+                              event.link,
+                              "_blank",
+                              "noopener noreferrer"
+                            )
+                          }
+                        >
+                          Entrar no contest
+                        </button>
+                      )}
+                    </div>
+                  </div>
+                </VerticalTimelineElement>
+              );
+            })}
+          </VerticalTimeline>
+        </>
+      }
+      </div>
+  ); 
 };
 
 function sortEvents(e1, e2) {
