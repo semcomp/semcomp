dc481a0bb4975d0b32df539ed051f801bf48071a
bielpieroli <joaogabrielpieroli18@gmail.com>
2026-05-04 00:01:19 -0300

    chore: FAQS adjusted

--- new-site/packages/front-site/src/lib/constants/FAQS.tsx
+++ new-site/packages/front-site/src/lib/constants/FAQS.tsx
@@ -1,23 +1,23 @@
 const FAQS = [
   {
-    Q: "O que é o tema da Semcomp?",
-    A: "A cada ano, a organização da Semcomp escolhe um tema para guiar o processo criativo da edição sendo desenvolvida. O tema escolhido direciona os elementos de marketing e da identidade visual, as atividades extracurriculares e culturais ao longo da semana e a ambientação do jogo, ajudando a criar uma atmosfera única e divertida para cada edição. Dessa forma, é importante esclarecer que as palestras, minicursos e atividades acadêmicas que acontecem ao longo da semana são independentes do tema. Essas atrações abordam conteúdos técnicos e de diversas áreas áreas da computação, sem relação com o tema escolhido para o conteúdo extracurricular."
+    Q: "O que é o tema da SEMCOMP?",
+    A: "A cada ano, a organização da SEMCOMP escolhe um tema para guiar o processo criativo da edição sendo desenvolvida. O tema escolhido direciona os elementos de marketing e da identidade visual, as atividades extracurriculares e culturais ao longo da semana e a ambientação do jogo, ajudando a criar uma atmosfera única e divertida para cada edição. Dessa forma, é importante esclarecer que as palestras, minicursos e atividades acadêmicas que acontecem ao longo da semana são independentes do tema. Essas atrações abordam conteúdos técnicos e de diversas áreas áreas da computação, sem relação com o tema escolhido para o conteúdo extracurricular."
   },
   {
     Q: "Como participar dos eventos?",
     A: "As inscrições são abertas online antes do evento."
   },
   {
-    Q: "A Semcomp ocorre apenas no segundo semestre?",
-    A: "Sim, tradicionalmente a Semcomp acontece no segundo semestre."
+    Q: "A SEMCOMP ocorre apenas no segundo semestre?",
+    A: "Sim, tradicionalmente a SEMCOMP acontece no segundo semestre, mas neste ano, teremos a SEMCOMP Beta no dia 16 de maio. A SEMCOMP Beta é uma versão reduzida do evento, com palestras e atividades culturais, que tem como objetivo oferecer uma experiência prévia da SEMCOMP para os participantes. A edição completa da SEMCOMP ocorrerá no segundo semestre, como de costume."
   },
    {
-      Q: "Por que se inscrever no site da Semcomp?",
+      Q: "Por que se inscrever no site da SEMCOMP?",
       A: "Com seu perfil de participante no site, você pode se inscrever nos minicursos, concursos e oficinas que serão oferecidos ao longo da semana. Além disso, é com os dados da inscrição que geramos os certificados de participação!"
     },
     {
-      Q: "Como a presença é coletada durante a Semcomp?",
-      A: "A presença é coletada por meio do escaneamento dos QR Codes de identificação disponíveis no perfil do participante no site e no crachá. Os QR Codes são escaneados na entrada e na saída das atrações ao longo do evento."
+      Q: "Como a presença é coletada durante a SEMCOMP?",
+      A: "A presença é coletada por meio do escaneamento dos QR Codes de identificação disponíveis no perfil do participante no site e no crachá. Os QR Codes são escaneados na entrada das atrações ao longo do evento."
     },
     {
       Q: "Qual a presença mínima no evento?",
@@ -25,22 +25,20 @@ const FAQS = [
     },
     {
       Q: "O que é overflow?",
-      A: "O Overflow é uma competição entre casas que ocorre durante toda a Semcomp. Todos os inscritos são separados em quatro casas e pontuam para sua casa a medida que participam de palestras, minicursos, concursos e atividades culturais. A casa com maior pontuação ao final do evento vence a Semcomp! "
+      A: "O Overflow é uma competição entre casas que ocorre durante toda a SEMCOMP. Todos os inscritos são separados em quatro casas e pontuam para sua casa a medida que participam de palestras, minicursos, concursos e atividades culturais. A casa com maior pontuação ao final do evento vence a SEMCOMP! "
     },
     {
       Q: "Como funciona o Coffe Break?",
-      A: "Os coffee breaks são “pausas para o café”, intervalos com comida que acontecem ao longo da semana do evento. Neste ano, o pacote de coffee breaks dará acesso aos quatro coffee breaks fechados da Semcomp, basta comprar o pacote em nosso site e apresentar suas credenciais na entrada. Além deles, organizaremos coffee breaks abertos ao longo da semana da Semcomp"
+      A: "Os coffee breaks são pausas de intervalos com comida que acontecem ao longo da semana entre eventos. Para a SEMCOMP Beta, teremos um coffee break no meio do dia, e para a edição completa da SEMCOMP, teremos muito mais!! Os coffee breaks são momentos de descontração e socialização entre os participantes, além de uma oportunidade para recarregar as energias com um lanche delicioso!"
     },
     {
-      Q: "A presença na Semcomp conta como Atividade Acadêmica Complementar (AAC)?",
-      A: "A presença na Semcomp concede aos inscritos um certficado de participação no evento, que pode ser contabilizado como Atividade Acadêmica Complementar (AAC). Também são emitidos certificados para os participantes em cada um dos minicursos."
+      Q: "A presença na SEMCOMP conta como Atividade Acadêmica Complementar (AAC)?",
+      A: "A presença na SEMCOMP concede aos inscritos um certficado de participação no evento, que pode ser contabilizado como Atividade Acadêmica Complementar (AAC). Também são emitidos certificados para os participantes em cada um dos minicursos."
     },
     {
       Q: "Como funciona a inscrição em minicursos?",
-      A: "A inscrição nos minicursos é feita através da página do participante em nosso site, de acordo com a disponibilidade de vagas em cada um deles. Vamos liberar as vagas em lotes, com horários diferentes, para que todos tenham oportunidade de participar! Anunciaremos os minicursos e as aberturas das inscrições em breve. "
+      A: "A inscrição nos minicursos é feita através da página dE perfil do participante em nosso site, de acordo com a disponibilidade de vagas em cada um deles. Vamos liberar as vagas em lotes, com horários diferentes, para que todos tenham oportunidade de participar! Fique atento às nossas redes sociais para não perder os anúncios de abertura de vagas!"
     }
-
-
 ]
 
 export default FAQS;
