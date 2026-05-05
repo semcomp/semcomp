b9968d43bacbaeb336a75280e1929f166f61f486
PedroHP1708 <pedrohpdias10@gmail.com>
2026-05-04 23:39:15 -0300

    fix: errors messages

--- new-site/packages/backend/internal/user/handler.go
+++ new-site/packages/backend/internal/user/handler.go
@@ -1,12 +1,15 @@
 package user
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/go-playground/validator/v10"
 )
 
 // UserHandler lida com as requisições HTTP para a entidade User.
@@ -24,17 +27,26 @@ func (h *UserHandler) CreateUser(c *gin.Context) {
 	var request CreateUserRequest
 
 	if err := c.ShouldBindJSON(&request); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "JSON inválido no corpo da requisição"})
-		return
+    	if unmarshalErr, ok := err.(*json.UnmarshalTypeError); ok {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": fmt.Sprintf("O campo '%s' recebeu um valor do tipo %s, mas esperava %s", 
+					unmarshalErr.Field, unmarshalErr.Value, unmarshalErr.Type),
+			})
+			return
+    	}
+
+		if validationErrs, ok := err.(validator.ValidationErrors); ok {
+			fieldErr := validationErrs[0]
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": fmt.Sprintf("Valor inválido para o campo '%s' (falhou na regra: %s)", 
+					fieldErr.Field(), fieldErr.Tag()),
+			})
+			return
+		}
 	}
 
 	safeUser, err := h.userService.CreateUser(request)
 	if err != nil {
-		var pgErr *pgconn.PgError
-		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
-			c.JSON(http.StatusConflict, gin.H{"error": "E-mail já cadastrado"})
-			return
-		}
 		if errors.Is(err, ErrEmailAlreadyExists) {
 			c.JSON(http.StatusConflict, gin.H{"error": "E-mail já cadastrado"})
 			return
@@ -118,9 +130,22 @@ func (h *UserHandler) UpdateUser(c *gin.Context) {
 
 	var request UpdateUserRequest
 	if err := c.ShouldBindJSON(&request); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Dados inválidos"})
-		return
-	}
+    	if unmarshalErr, ok := err.(*json.UnmarshalTypeError); ok {
+        c.JSON(http.StatusBadRequest, gin.H{
+            "error": fmt.Sprintf("O campo '%s' recebeu um valor do tipo %s, mas esperava %s", 
+                unmarshalErr.Field, unmarshalErr.Value, unmarshalErr.Type),
+        })
+        return
+    }
+
+    if validationErrs, ok := err.(validator.ValidationErrors); ok {
+        fieldErr := validationErrs[0]
+        c.JSON(http.StatusBadRequest, gin.H{
+            "error": fmt.Sprintf("Valor inválido para o campo '%s' (falhou na regra: %s)", 
+                fieldErr.Field(), fieldErr.Tag()),
+        })
+        return
+    }
 
 	if err := h.userService.UpdateUser(uint(id), request); err != nil {
 		// TODO: Pensar se é o melhor jeito de fazer isso. Há a possibilidade de
@@ -130,7 +155,7 @@ func (h *UserHandler) UpdateUser(c *gin.Context) {
 		if errors.As(err, &pgErr) {
 			// Código de chave duplicada, no caso, a única possível é a de email
 			if pgErr.Code == "23505" {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "Email já cadastrado para outro usuário"})
+				c.JSON(http.StatusConflict, gin.H{"error": "Email já cadastrado para outro usuário"})
 				return
 			}
 		}
@@ -138,6 +163,7 @@ func (h *UserHandler) UpdateUser(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "Usuário atualizado com sucesso!"})
+}
 }
 
 // DeleteUser remove um usuário do sistema identificando-o pelo ID na URL.
