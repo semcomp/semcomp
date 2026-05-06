ee6a7ccd3070df20eb01e9609fbfb4ad388713cc
bielpieroli <joaogabrielpieroli18@gmail.com>
2026-05-06 08:29:37 -0300

    fix: front and backoffice building errors

--- new-site/packages/front-backoffice/src/mock/user-backoffice.ts
+++ new-site/packages/front-backoffice/src/mock/user-backoffice.ts
@@ -1,16 +1,16 @@
 import type { BackofficeUserType } from "@/types/BackofficeUserType";
 
 export const sampleBackofficeUsers: BackofficeUserType[] = [
-  { id: "1", name: "Ana Beatriz Santos", email: "ana@semcomp.com", password: "123456" },
-  { id: "2", name: "Carlos Eduardo Lima", email: "carlos@semcomp.com", password: "123456" },
-  { id: "3", name: "Fernanda Oliveira", email: "fernanda@semcomp.com", password: "123456" },
-  { id: "4", name: "Gabriel Costa", email: "gabriel@semcomp.com", password: "123456" },
-  { id: "5", name: "Helena Martins", email: "helena@semcomp.com", password: "123456" },
-  { id: "6", name: "Igor Ferreira", email: "igor@semcomp.com", password: "123456" },
-  { id: "7", name: "Juliana Ramos", email: "juliana@semcomp.com", password: "123456" },
-  { id: "8", name: "Lucas Almeida", email: "lucas@semcomp.com", password: "123456" },
-  { id: "9", name: "Marina Pereira", email: "marina@semcomp.com", password: "123456" },
-  { id: "10", name: "Nicolas Barbosa", email: "nicolas@semcomp.com", password: "123456" },
-  { id: "11", name: "Olívia Souza", email: "olivia@semcomp.com", password: "123456" },
-  { id: "12", name: "Pedro Henrique Nunes", email: "pedro@semcomp.com", password: "123456" },
+  { id: "1", email: "ana@semcomp.com", password: "123456" },
+  { id: "2", email: "carlos@semcomp.com", password: "123456" },
+  { id: "3", email: "fernanda@semcomp.com", password: "123456" },
+  { id: "4", email: "gabriel@semcomp.com", password: "123456" },
+  { id: "5", email: "helena@semcomp.com", password: "123456" },
+  { id: "6", email: "igor@semcomp.com", password: "123456" },
+  { id: "7", email: "juliana@semcomp.com", password: "123456" },
+  { id: "8", email: "lucas@semcomp.com", password: "123456" },
+  { id: "9", email: "marina@semcomp.com", password: "123456" },
+  { id: "10", email: "nicolas@semcomp.com", password: "123456" },
+  { id: "11", email: "olivia@semcomp.com", password: "123456" },
+  { id: "12", email: "pedro@semcomp.com", password: "123456" },
 ];
