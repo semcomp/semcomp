00ebe78059d45664cb3c7118e0992c1eded06142
Pedro Paffaro <pedroafpaffaro@gmail.com>
2026-07-06 09:47:11 -0300

    Ajuste no docker-compose e Makefile

---
+++ new-site/packages/front-site/src/constants/ApiURL 2.tsx
@@ -0,0 +1,9 @@
+import { DEBUGMODE } from "./DebugMode";
+
+
+const ACTUAL_URL = DEBUGMODE 
+  ? "http://localhost:4000" 
+  : "https://semcomp.icmc.usp.br/api"; 
+
+export const BASEURL = ACTUAL_URL;
+export default ACTUAL_URL;
