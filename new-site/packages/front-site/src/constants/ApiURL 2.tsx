3f6e3745b8155bd870b9b3240a1b888187548524
João Gabriel Pieroli da Silva <joaogabrielpieroli18@gmail.com>
2026-07-13 14:09:07 -0300

    Delete new-site/packages/front-site/src/constants/ApiURL 2.tsx

--- new-site/packages/front-site/src/constants/ApiURL 2.tsx
+++
@@ -1,9 +0,0 @@
-import { DEBUGMODE } from "./DebugMode";
-
-
-const ACTUAL_URL = DEBUGMODE 
-  ? "http://localhost:4000" 
-  : "https://semcomp.icmc.usp.br/api"; 
-
-export const BASEURL = ACTUAL_URL;
-export default ACTUAL_URL;
